58f90e15a60d7383df887a01631827e4e1585e8c
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2013-08-19 15:33:16 -0400

    Add usage text for 'Add'

--- hdata.hs
+++ hdata.hs
@@ -16,6 +16,17 @@ data Operation = Add
 
 usage :: Operation -> String
 
+usage Add = "usage: " ++ progName ++ " add <filters>\n\
+            \filters:\n\
+            \    -f <file>\n\
+            \    -t <title>\n\
+            \    -a <author1 [author2] ...>\n\
+            \    -k <keyword1 [keyword2] ...>\n\
+            \    -j <journal>\n\
+            \    -i <issue>\n\
+            \    -d <date>\n\
+            \    -p <page-from>-<page-to>"
+
 usage Help = "usage: " ++ progName ++ " [operation] [id]\n\
              \operations:\n\
              \    add                  <filters>     \n\
@@ -26,7 +37,7 @@ usage Help = "usage: " ++ progName ++ " [operation] [id]\n\
              \    remove   [options]             <id>\n\    
              \    search   [options]   [filters] [id]\n\    
              \    view     [options]             <id>\n\    
-             \    version\n"   
+             \    version"   
 
 usage op = show op ++ " not yet implemented" 
 
