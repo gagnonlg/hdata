5ff238dbb8b5cd7f19863e4d72df5d4157add284
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2013-08-19 23:59:53 -0400

    Add.hs: adds flags parsing

--- Add.hs
+++ Add.hs
@@ -3,10 +3,58 @@ module Add (
     usageAdd
 ) where
 
+import Data.List
 import Util
 
+data Flag = Path String
+          | Title String
+          | Authors String
+          | Keywords String
+          | Journal String
+          | Issue String
+          | Date String
+          | Pages String
+          deriving (Show)
+
+isFlag :: String -> Bool
+isFlag f = f `elem` ["-f","-p","-t","-j","-i","-d","-a","-k"]
+
 add :: [String] -> IO ()
-add argv = putStrLn $ usageAdd
+add [] = error $ "add: no arguments specified ('" ++ progName ++ " add help' for help)"
+add argv = if isHelp $ head argv 
+    then do putStrLn usageAdd 
+    else do 
+        case parseFlags argv of
+            Left  msg   -> error $ "add: " ++ msg
+            Right flags -> error "args were parsed OK, but the rest of the module is not done"
+
+parseFlags :: [String] -> Either String [Flag]
+parseFlags argv = parseFlags' [] argv
+    where parseFlags' _  (x:[]) = Left "too few arguments"
+          parseFlags' fs []     = Right fs
+          parseFlags' fs xs     = 
+              let flag = getFlag xs
+              in case flag of
+                  Left  msg  -> Left msg
+                  Right f    -> parseFlags' (f:fs) (dropWhile (not . isFlag) (tail xs))
+
+getFlag :: [String] -> Either String Flag
+getFlag x@(x0:x1:_) = 
+    let flag = case x0 of
+                   "-f" -> Path    x1
+                   "-t" -> Title   x1
+                   "-j" -> Journal x1
+                   "-i" -> Issue   x1
+                   "-d" -> Date    x1
+                   "-p" -> Pages   x1
+                   "-k" -> Keywords $ getValues $ tail  x
+                   "-a" -> Authors  $ getValues $ tail  x
+    in if isFlag x1 
+           then Left "too few argument"
+           else Right flag 
+
+getValues :: [String] -> String
+getValues argv = intercalate "/" $ takeWhile (not . isFlag) argv 
 
 usageAdd :: String
 usageAdd = "usage: " ++ progName ++ " add <filters>\n\
