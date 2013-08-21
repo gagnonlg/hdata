5515feb4e8a211f191a9af62e5d76cdbacdfd4a9
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2013-08-21 10:33:17 -0400

    Search.hs: Add usage text

--- Search.hs
+++ Search.hs
@@ -1,8 +1,25 @@
 module Search (
-    search
+    search,
+    usageSearch
 ) where
 
 import Util
 
 search :: [String] -> IO ()
-search argv = putStrLn $ usage Search
+search argv = putStrLn $ usageSearch
+
+usageSearch :: String
+usageSearch = "usage: " ++ progName ++ " search <filters>\n\
+              \or:\n\
+              \       " ++ progName ++ " search [id]\n\
+              \filters:\n\
+              \    -f <file>\n\
+              \    -t <title>\n\
+              \    -a <author1 [author2] ...>\n\
+              \    -k <keyword1 [keyword2] ...>\n\
+              \    -j <journal>\n\
+              \    -v <volume>\n\
+              \    -i <issue>\n\
+              \    -d <date-from> <date-to>\n\
+              \    -p <page-from> <page-to>\n\
+              \    -b : bookmarks"
